365f5f4b3390c2ddaa020581197c40f52c628337
TeoVH <villadamateo64@gmail.com>
2023-01-29 18:44:43 -0500

    0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function that takes a float "a" and a float "b"
+as arguments and returns their sum as a float
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Function that add two float
+    Return: the sum as a float
+    """
+    return a + b
