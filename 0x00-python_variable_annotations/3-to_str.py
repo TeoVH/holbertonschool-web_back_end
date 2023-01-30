5a6fc0cf32cd29ce32b07a5699ee87cb86a4f28e
TeoVH <villadamateo64@gmail.com>
2023-01-29 22:20:56 -0500

    0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Function to_str that takes a float n as argument and
+returns the string representation of the floatBasic
+annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns the str representations of n
+    """
+    return str(n)
