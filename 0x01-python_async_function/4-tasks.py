b31ecbbe9c434f99b5afc2bd172bdf8e0815c313
TeoVH <villadamateo64@gmail.com>
2023-01-29 23:01:58 -0500

    0x01. Python - Async

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,27 +1,19 @@
 #!/usr/bin/env python3
-"""
-Take the code from wait_n and alter it into a new function task_wait_n.
-The code is nearly identical to wait_n except task_wait_random is being called
-"""
+""" Take the code from wait_n and alter it into a new function task_wait_n.
+    The code is nearly identical to wait_n except task_wait_random is being
+    called. """
+import asyncio
 from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
-task_wait_random = __import__("3-tasks").task_wait_random
 
-
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """
-    Return the list of all the delays (float values)
-    The list of the delays should be in ascending order without using sort()
-    because of concurrency
-    """
-    results = []
-    promises = []
-    for _ in range(0, n):
-        task = task_wait_random(max_delay)
-        task.add_done_callback(lambda x: results.append(x.result()))
-        promises.append(task)
-
-    for promise in promises:
-        await promise
-
-    return results
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ Tasks """
+    delays: List[float] = []
+    all_delays: List[float] = []
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+    for delay in asyncio.as_completed(delays):
+        earliest_result = await delay
+        all_delays.append(earliest_result)
+    return all_delays
