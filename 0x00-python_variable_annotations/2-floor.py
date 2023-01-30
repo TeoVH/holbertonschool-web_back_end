d40239098a8fdcdabab731c848f0c299c8da1d0e
TeoVH <villadamateo64@gmail.com>
2023-01-29 22:17:44 -0500

    0x00. Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function floor which takes a float n as argument
+and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function that returns the floor of n
+    """
+    return int(n)
