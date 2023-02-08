365f982d669faac599026c8a0350a821c200fb5e
TeoVH <villadamateo64@gmail.com>
2023-02-07 23:35:45 -0500

    Error handler: Forbidden

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -14,16 +14,26 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
-@app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(401)
+@app.errorhandler(403)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+    Forbidden
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """
+    Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
